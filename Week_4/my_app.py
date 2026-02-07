9b63bfa199a258b700500d437e51a4a48f137b4d
rburkman777 <rburkman@nd.edu>
2026-02-07 16:03:47 -0500

    added bar chart

--- Week_4/my_app.py
+++ Week_4/my_app.py
@@ -1,19 +1,12 @@
 import streamlit as st
 # Markdown Hashtag 
-st.title("Hello, Streamlit!")
-st.markdown("# Hello, streamlit")
+st.title("Congressional Districts in the 2022 Election")
 
 
-st.write("This is my first Streamlit app.")
-
-color = st.color_picker("Pick a color", "#00f900")
-st.write(f"You picked: {color}")
-
-if st.button("Click me!"):
-    st.write("You clicked the button")
-else:
-    ("Click the button, see what happens...")
-
+st.write("Welcome to my first Streamlit app! As a political science major,"
+"I believe there is a lot to be learned from studying election results. This "
+"page was designed to let you interact with this data from FiveThirtyEight and explore"
+" trends in American elections.")
 
 import pandas as pd
 
@@ -27,19 +20,17 @@ df = pd.read_csv("urbanization-index-2022.csv")
 st.write("Here's our data!")
 st.dataframe(df)
 
-
-box_plot1 = sns.boxplot(x = df["urban"], y=df["pvi_22"])
-st.pyplot(box_plot1.get_figure())
-
+st.write("Use this menu to look at individual distirct types based on population density")
 grouping = st.selectbox("Select a district type", df["grouping"].unique(), index = None)
 filtered_df = df[df["grouping"] == grouping]
 
 st.write(f"People in the {grouping}")
 st.dataframe(filtered_df)
 
-st.bar_chart(df["Urban"])
+st.bar_chart(df, x = 'pvi_22', y = 'grouping')
 
-
+box_plot1 = sns.boxplot(x = df["urban"], y=df["pvi_22"])
+st.pyplot(box_plot1.get_figure())
 
 st.pyplot(box_plot1.get_figure())
 
