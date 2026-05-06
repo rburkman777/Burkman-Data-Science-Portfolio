0a59c5ee37d4f2a58a8e01e91315cb5e220a2f3c
Rex Burkman <rburkman@nd.edu>
2026-05-06 16:33:49 -0400

    Update unsupervised_machine_learning_app_code.py

--- MLUnsupervisedApp/unsupervised_machine_learning_app_code.py
+++ MLUnsupervisedApp/unsupervised_machine_learning_app_code.py
@@ -308,22 +308,6 @@ if model_type == "Hierarchical Clustering":
 
     with st.expander("CLICK HERE to learn more about the dendrogram"):
         st.write(
-        "The dendrogram is a visualization of our tree-structured model and shows how data points are split into clusters. "
-        "The vertical height represents distance between clusters while the horizontal axis shows data points. "
-        "You can use this to help decide the number of clusters (k)."
-    )
-
-st.markdown("-----------------------------------------------------------------")
-
-    st.write(
-        "This is a visualization of the model based on the inputs you have given thus far. "
-        "You will notice that it branches out into clusters!"
-    )
-
-    st.pyplot(fig)
-
-    with st.expander("CLICK HERE to learn more about the dendrogram"):
-        st.write(
             "The dendrogram is a visualization of our tree-strcutured model and shows how data points are split into clusters. You will notice that the various branches of the dendrogram resemble clusters. "
             "The vertical height represents distance between clusters while the horizontal access features indicators of data points. "
             "You can use this to help decide the number of clusters (k)."
